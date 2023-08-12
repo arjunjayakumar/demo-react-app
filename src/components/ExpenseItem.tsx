05bccbaa2fc8fd98409bbad423e6b90b79005502
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-12 18:30:56 +0530

    Add new values

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,7 +1,11 @@
 export default function ExpenseItem() {
   return (
     <>
-      <h1>Expense Item!</h1>
+      <div className="type">12th August 2023</div>
+      <div className="contents">
+        <h2>Udemy Charges</h2>
+        <div className="amount">$389</div>
+      </div>
     </>
   );
 }
