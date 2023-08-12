d89375ada2b27b1d2d9b93608ee1f2b60aaa0ed1
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-12 18:52:50 +0530

    Add styles

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,11 +1,13 @@
+import "./ExpenseItem.scss";
+
 export default function ExpenseItem() {
   return (
-    <>
-      <div className="type">12th August 2023</div>
-      <div className="contents">
+    <div className="expense-item">
+      <div>12th August 2023</div>
+      <div className="expense-item__description">
         <h2>Udemy Charges</h2>
-        <div className="amount">$389</div>
+        <div className="expense-item__price">$389.99</div>
       </div>
-    </>
+    </div>
   );
 }
