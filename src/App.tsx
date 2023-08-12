0b642c30bec261367c779476e72bdc93c6c61c40
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-12 19:46:57 +0530

    Pass data via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import "./App.scss";
 import ExpenseItem from "./components/ExpenseItem";
 
+export class ExpenseObject {
+  public id: string = "";
+  public title: string = "";
+  public amount: number = 0;
+  public date: Date = new Date();
+}
+
 function App() {
+  const expenses: ExpenseObject[] = [
+    {
+      id: "e1",
+      title: "Health Insurance",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Home Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Investment",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <>
-      <ExpenseItem />
+      {expenses.map((expense) => (
+        <ExpenseItem expense={expense} key={expense.id} />
+      ))}
     </>
   );
 }
--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,12 +1,21 @@
+import { ExpenseObject } from "../App";
 import "./ExpenseItem.scss";
 
-export default function ExpenseItem() {
+class Expense {
+  public expense = new ExpenseObject();
+}
+
+export default function ExpenseItem(props: Expense) {
   return (
+    // Expense Item Container
     <div className="expense-item">
-      <div>12th August 2023</div>
+      {/* Date */}
+      <div>{props.expense.date.toDateString()}</div>
+
+      {/* Description */}
       <div className="expense-item__description">
-        <h2>Udemy Charges</h2>
-        <div className="expense-item__price">$389.99</div>
+        <h2>{props.expense.title}</h2>
+        <div className="expense-item__price">${props.expense.amount}</div>
       </div>
     </div>
   );
