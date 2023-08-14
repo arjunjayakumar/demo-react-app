e4d116b50241fb367cbd56172eabc81fa5a02aa4
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-14 18:38:27 +0530

    Use Object destructuring

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -5,17 +5,17 @@ class ExpenseProps {
   public expense = new Expense();
 }
 
-export default function ExpenseItem(props: ExpenseProps) {
+export default function ExpenseItem({ expense }: ExpenseProps) {
   return (
     // Expense Item Container
     <div className="expense-item">
       {/* Date */}
-      <div>{props.expense.date.toDateString()}</div>
+      <div>{expense.date.toDateString()}</div>
 
       {/* Description */}
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
-        <div className="expense-item__price">${props.expense.amount}</div>
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">${expense.amount}</div>
       </div>
     </div>
   );
