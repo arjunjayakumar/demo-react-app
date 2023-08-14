94b8663adf274c9f73dc316d24a3743c399c3c78
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-14 18:58:44 +0530

    Add date detials

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -6,12 +6,20 @@ class ExpenseProps {
 }
 
 export default function ExpenseItem({ expense }: ExpenseProps) {
-  const { title, date, amount } = expense;
+  const { title, date, amount } = expense,
+    month = date.toLocaleString("en-US", { month: "long" }),
+    day = date.toLocaleString("en-US", { day: "2-digit" }),
+    year = date.getFullYear();
+
   return (
     // Expense Item Container
     <div className="expense-item">
       {/* Date */}
-      <div>{date.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
 
       {/* Description */}
       <div className="expense-item__description">
