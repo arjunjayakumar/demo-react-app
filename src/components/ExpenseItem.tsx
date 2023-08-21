97cdcfd35c415b1e3d13bcab78cac370bc19e217
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-21 19:10:14 +0530

    Separate date component

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,25 +1,14 @@
 import { Expense } from "../App";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.scss";
 
-class ExpenseProps {
-  public expense = new Expense();
-}
-
-export default function ExpenseItem({ expense }: ExpenseProps) {
-  const { title, date, amount } = expense,
-    month = date.toLocaleString("en-US", { month: "long" }),
-    day = date.toLocaleString("en-US", { day: "2-digit" }),
-    year = date.getFullYear();
+export default function ExpenseItem({ expense }: { expense: Expense }) {
+  const { title, date, amount } = expense;
 
   return (
     // Expense Item Container
     <div className="expense-item">
-      {/* Date */}
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={date} />
 
       {/* Description */}
       <div className="expense-item__description">
