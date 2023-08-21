ca7b7e96642446ac9b4e67ebb6a2d9bb60d70677
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-21 19:15:28 +0530

    Add styles to date

--- src/components/ExpenseDate.tsx
+++ src/components/ExpenseDate.tsx
@@ -1,13 +1,15 @@
+import "./ExpenseDate.scss";
+
 export default function ExpenseDate({ date }: { date: Date }) {
   const month = date.toLocaleString("en-US", { month: "long" }),
     day = date.toLocaleString("en-US", { day: "2-digit" }),
     year = date.getFullYear();
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 }
