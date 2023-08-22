a20cd6cfa5d2a24e03bea9c3f09286e3f9739d1c
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-08-22 18:54:26 +0530

    Add expenses styles

--- src/components/Expenses.tsx
+++ src/components/Expenses.tsx
@@ -1,8 +1,15 @@
 import { Expense } from "../App";
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.scss";
 
 const Expenses = ({ expensesArray }: { expensesArray: Expense[] }) => {
-  return expensesArray.map((expense) => <ExpenseItem expense={expense} />);
+  return (
+    <div className="expenses">
+      {expensesArray.map((expense) => (
+        <ExpenseItem expense={expense} key={expense?.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Expenses;
